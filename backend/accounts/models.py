fa481c8b571deae7606b7cb0271dfec8d1291af0
andrew <andry925@ukr.net>
2024-03-11 20:19:44 +0200

    feat: add representation for userprofile

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -45,3 +45,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}'
