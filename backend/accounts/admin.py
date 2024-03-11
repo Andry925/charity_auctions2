43032e9db7bcae046ae36bf95c1cd083ef380567
andrew <andry925@ukr.net>
2024-03-11 20:20:40 +0200

    feat: add admin page to display models

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User, UserProfile
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_superuser")
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
