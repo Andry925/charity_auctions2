081f7a506359fd44401009fff642eeaf69d9f941
andrew <andry925@ukr.net>
2024-03-13 21:42:44 +0200

    feat: add neccesary urls for login and logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout')
 ]
