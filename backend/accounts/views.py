3d95087c8272f35d698084db47ccecc3c1eeff66
andrew <andry925@ukr.net>
2024-03-13 21:27:22 +0200

    feat: implement token-based authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,10 @@
 from rest_framework import permissions, status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer
+from django.contrib.auth import login
+from .serializers import UserSerializer, LoginSerializer
 
 
 class CreateUserView(APIView):
@@ -18,3 +20,19 @@ class CreateUserView(APIView):
                     serializer.data,
                     status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginUserView(APIView):
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        data = request.data
+        serializer = LoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True) and not request.user.is_authenticated:
+            user = serializer.login_user(data=data)
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
