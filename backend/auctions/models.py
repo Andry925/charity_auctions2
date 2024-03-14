008025bff5a663347bc15679c66271dfd8108bb9
andrew <andry925@ukr.net>
2024-03-14 22:37:03 +0200

    feat: write functionality to calculate expiration of an auction

--- backend/auctions/models.py
+++ backend/auctions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, timedelta
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,6 +14,19 @@ class Auction(models.Model):
         default=0, validators=[
             MinValueValidator(1), MaxValueValidator(20)])
     image_url = models.ImageField(upload_to=upload_to, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     auto_now_add = models.DateTimeField(auto_now=True)
     expires_at = models.DateTimeField(default=None, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.expires_at = self.calculate_expired_at()
+        super().save(*args, **kwargs)
+
+    def calculate_expired_at(self):
+        self.created_at = datetime.now()
+        expire_data = self.created_at +\
+            timedelta(days=self.auction_duration)
+        return expire_data
+
+    def __str__(self):
+        return self.description
