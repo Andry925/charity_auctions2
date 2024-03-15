04f4fbb3d5917dadb00604c831d4b2fc2b6c84eb
andrew <andry925@ukr.net>
2024-03-15 11:37:16 +0200

    feat: write serializers for auctions

---
+++ backend/auctions/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Auction
+
+
+class AuctionSerializer(serializers.ModelSerializer):
+    image_url = serializers.ImageField(required=False)
+
+    class Meta:
+        model = Auction
+        fields = '__all__'
