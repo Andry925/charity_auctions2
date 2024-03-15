e47841406ba46dded41e6697107ec6892fceafbc
andrew <andry925@ukr.net>
2024-03-15 12:01:57 +0200

    feat: update user field in Auction table

--- backend/auctions/serializers.py
+++ backend/auctions/serializers.py
@@ -4,6 +4,7 @@ from .models import Auction
 
 class AuctionSerializer(serializers.ModelSerializer):
     image_url = serializers.ImageField(required=False)
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Auction
