c50bab728e39ca12296e37a5e483bd5aced6753f
andrew <andry925@ukr.net>
2024-03-16 12:54:30 +0200

    feat: add urls configuration

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-]
+    path(
+        'all_auctions/',
+        views.AuctionListView.as_view(),
+        name='all_auctions'),
+    path(
+        'particular_auction/<int:pk>/',
+        views.AuctionDetailView.as_view(),
+        name='create_auction')]
