729dea0e3b4b1609c27441d9ce107316aa19cd59
andrew <andry925@ukr.net>
2024-03-16 12:54:00 +0200

    feat: implement CRUDendpoints for auctions

--- backend/auctions/views.py
+++ backend/auctions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
+from .models import Auction
+from .serializers import AuctionSerializer
 
-# Create your views here.
+
+class AuctionListView(generics.ListCreateAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionSerializer
+    parser_classes = [MultiPartParser, FormParser]
+    permissions = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class AuctionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionSerializer
+    parser_classes = [MultiPartParser, FormParser]
+    permissions = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
