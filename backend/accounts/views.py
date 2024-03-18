01057765b4530aa2a65f738afaf1bc6dbff7a8dd
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-03-18 22:45:01 +0200

    feat:restrict non owner to edit auction (#6)
    * refactor: add neccesary spaces between functions
    
    * feat: restrict non-user to edit auction
    
    * refactor: change signature of update method

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import login,logout
+from django.contrib.auth import login
 from .serializers import UserSerializer, LoginSerializer
 
 
@@ -21,6 +21,7 @@ class CreateUserView(APIView):
                     status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LoginUserView(APIView):
     permission_classes = [permissions.AllowAny]
     authentication_classes = [TokenAuthentication]
@@ -28,7 +29,8 @@ class LoginUserView(APIView):
     def post(self, request):
         data = request.data
         serializer = LoginSerializer(data=data)
-        if serializer.is_valid(raise_exception=True) and not request.user.is_authenticated:
+        if serializer.is_valid(
+                raise_exception=True) and not request.user.is_authenticated:
             user = serializer.login_user(data=data)
             token, created = Token.objects.get_or_create(user=user)
             login(request, user)
@@ -39,9 +41,7 @@ class LoginUserView(APIView):
 class LogoutUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+
     def get(self, request, format=None):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
-
-
-
