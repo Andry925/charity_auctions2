23615a3015fef3409df71ded071495ef6ad2c283
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-03-24 20:11:46 +0200

    feat: implement custom paginator for auctions (#9)
    * chore: setting up custom paginator class
    
    * feat: create custom paginator class

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
     'auctions',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
