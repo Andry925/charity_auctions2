26eb5bb1b0d902ad25b4953dd8d2133e5df10176
andrew <andry925@ukr.net>
2024-04-25 21:37:31 +0300

    feat: implement bid validation in serializer

--- backend/bid/serializers.py
+++ backend/bid/serializers.py
@@ -8,3 +8,16 @@ class BidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
         fields = '__all__'
+
+    def validate(self, validated_data):
+        start_price = float(self.context.get('auction_model').starting_price)
+        current_bid = float(self.context.get('auction_model').bids.bid_amount)
+        offered_bid = float(validated_data.get('bid_amount'))
+
+        if offered_bid and start_price >= offered_bid:
+            raise serializers.ValidationError(
+                'Bid can not be less than start_price')
+        elif current_bid and current_bid >= offered_bid:
+            raise serializers.ValidationError(
+                'Offered bid can not be less than current_bid')
+        return validated_data
