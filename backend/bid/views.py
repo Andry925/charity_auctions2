1273fec70ff09848d21559e18a346e49482984e4
andrew <andry925@ukr.net>
2024-05-26 14:59:00 +0300

    fix: solve n+1 problem in bid endpoints

--- backend/bid/views.py
+++ backend/bid/views.py
@@ -4,14 +4,16 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from auctions.models import Auction
 from .models import Bid
 from .serializers import BidSerializer
 
 
-class BidCreateView(APIView):
+class BidCreateView(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BidSerializer
 
     def post(self, request, pk):
         auction_model = self.validate_auction_bid(request=request, pk=pk)
@@ -38,6 +40,6 @@ class BidCreateView(APIView):
 
 class BidListView(APIView):
     def get(self, request):
-        auctions = Bid.objects.all()
+        auctions = Bid.objects.select_related("bidder")
         serializer = BidSerializer(auctions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
