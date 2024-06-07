27c4ff78c5d5d12d6c82012a88acc129716d462b
andrew <andry925@ukr.net>
2024-06-07 22:24:43 +0300

    tests: implement initial test

--- backend/bid/tests.py
+++ backend/bid/tests.py
@@ -16,7 +16,6 @@ class TestBidAuctionSetup(APITestCase):
         self.initial_cleaned_dict_data = self.parse_test_config_file()
         self.cleaned_dict_data = self.remove_extra_keys_from_dict(
             self.initial_cleaned_dict_data)
-        print(self.cleaned_dict_data)
         self.first_user = get_user_model().objects.create_user(
             **self.cleaned_dict_data.get("user_credentials"))
         self.second_user = get_user_model().objects.create_user(
@@ -76,8 +75,28 @@ class TestBidEndpoints(TestBidAuctionSetup):
         self.assertEqual(
             cleaned_response,
             {
-                'id': 1,
                 'bidder': 'tester1232@gmail.com',
                 'auction': 'test description',
                 'bid_amount': '15.00',
                 'description': None})
+
+    def test_bid_is_created(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token_second_user.key)
+        self.cleaned_dict_data.get("bid_data")["bid_amount"] = 20
+        response = self.client.post(reverse('manage_bids', args=[self.auction.id]), data=self.cleaned_dict_data.get("bid_data"))
+        current_price = float(json.loads(response.content)["bid_amount"])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(current_price, 20.00)
+
+    def test_try_bid_own_auction(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token_first_user.key)
+        self.cleaned_dict_data.get("bid_data")["bid_amount"] = 20
+        with self.assertRaises(ValueError) as e:
+            self.client.post(reverse('manage_bids', args=[self.auction.id]),
+                                    data=self.cleaned_dict_data.get("bid_data"))
+        self.assertEqual("You can not bid your own auctions", e.exception.args[0])
+
+
+
+
+
