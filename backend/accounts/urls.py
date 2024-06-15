d57c6728da6f5def7164c8b2f960c1214f9b3c09
andrew <andry925@ukr.net>
2024-06-15 11:20:48 +0300

    chore: add neccesary jwt configuration

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('profile/', views.ViewMyProfileView.as_view(), name='profile')
 ]
