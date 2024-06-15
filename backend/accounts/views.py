4b151a19999101ac7e84cc8c00abaa4a68896134
andrew <andry925@ukr.net>
2024-06-15 11:21:39 +0300

    feat: implement-jwt-authentication-endpoints

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,11 +1,10 @@
 from rest_framework import permissions, status
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import login
 from django.contrib.auth import update_session_auth_hash
-from .serializers import UserSerializer, LoginSerializer, UserProfileSerializer, ResetPasswordEmailSerializer, ChangePasswordSerializer
+from .serializers import UserSerializer, LoginSerializer, UserProfileSerializer, ChangePasswordSerializer
 from .models import UserProfile
 
 
@@ -26,7 +25,6 @@ class CreateUserView(APIView):
 
 class LoginUserView(APIView):
     permission_classes = [permissions.AllowAny]
-    authentication_classes = [TokenAuthentication]
 
     def post(self, request):
         data = request.data
@@ -34,24 +32,31 @@ class LoginUserView(APIView):
         if serializer.is_valid(
                 raise_exception=True) and not request.user.is_authenticated:
             user = serializer.login_user(data=data)
-            token, created = Token.objects.get_or_create(user=user)
+            token = RefreshToken.for_user(user)
+            response_data = serializer.data
+            response_data['tokens'] = {
+                "refresh": str(token), "access": str(
+                    token.access_token)}
             login(request, user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response(response_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LogoutUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
 
-    def get(self, request, format=None):
-        request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+    def post(self, request, format=None):
+        try:
+            refresh_token = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class ViewMyProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
 
     def get(self, request):
         my_profile = UserProfile.objects.filter(user=request.user)
@@ -61,14 +66,17 @@ class ViewMyProfileView(APIView):
 
 class ResetPasswordView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
 
     def post(self, request):
         serializer = ChangePasswordSerializer(data=request.data)
         user = request.user
-        if serializer.is_valid(raise_exception=True) and user.check_password(serializer.data.get('old_password')):
+        if serializer.is_valid(
+                raise_exception=True) and user.check_password(
+                serializer.data.get('old_password')):
             user.set_password(serializer.data.get('new_password'))
             user.save()
             update_session_auth_hash(request, user)
-            return Response({"message":"Password changes succesfuly"}, status=status.HTTP_200_OK)
-        return Response({"error":"incorrect password"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"message": "Password changes succesfuly"}, status=status.HTTP_200_OK)
+        return Response({"error": "incorrect password"},
+                        status=status.HTTP_400_BAD_REQUEST)
