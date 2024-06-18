343052fb977bd3660586c3056eb951c558b7104d
andrew <andry925@ukr.net>
2024-06-18 14:48:56 +0300

    feat: implement-models-for-chat

--- backend/websocket_chat/models.py
+++ backend/websocket_chat/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Conversation(models.Model):
+    initiator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='conv_initiator')
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='conv_reciever')
+    start_time = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='message_sender')
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.SET_NULL,
+        related_name='conversation_messages')
+    text = models.TextField(max_length=1024, null=True, blank=True)
+    attached_file = models.FileField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
