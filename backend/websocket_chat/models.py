a0bffb698cc5a7d130e791df7d41629362867f0a
andrew <andry925@ukr.net>
2024-06-22 16:38:55 +0300

    feat: implement endpoints for web-socket chat

--- backend/websocket_chat/models.py
+++ backend/websocket_chat/models.py
@@ -6,11 +6,11 @@ class Conversation(models.Model):
     initiator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        related_name='conv_initiator')
+        related_name='conv_initiator', null=True)
     receiver = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        related_name='conv_reciever')
+        related_name='conv_reciever', null=True)
     start_time = models.DateTimeField(auto_now_add=True)
 
 
@@ -18,11 +18,11 @@ class Message(models.Model):
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        related_name='message_sender')
+        related_name='message_sender', null=True)
     conversation = models.ForeignKey(
         Conversation,
         on_delete=models.SET_NULL,
-        related_name='conversation_messages')
+        related_name='conversation_messages', null=True)
     text = models.TextField(max_length=1024, null=True, blank=True)
     attached_file = models.FileField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
