768711155f14516ca61af2288370c252651620a0
andrew <andry925@ukr.net>
2024-06-29 14:10:16 +0300

    chore: add cache-redis configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,6 +106,13 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
