887c208a0c1f7960f1a208efa7b0ec6d42c7519c
andrew <andry925@ukr.net>
2024-06-29 15:26:27 +0300

    feat: add signal to clear cash after delete/update/create

--- backend/auctions/signals.py
+++ backend/auctions/signals.py
@@ -1,11 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
-from django.conf import settings
 from .models import Auction
 
-
 @receiver(post_save, sender=Auction)
-def clear_cache_after_auction_creation(instance, created, **kwargs):
-    if created:
-        cache.delete(settings.AUCTION_CACHE_NAME)
+@receiver(post_delete, sender=Auction)
+def clear_cache_after_auction_change(instance, **kwargs):
+    cache.clear()
