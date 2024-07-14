901b16589956ad14c4a5012fb466fb1c97a0c39c
andrew <andry925@ukr.net>
2024-07-14 16:49:18 +0300

    refactor: select REST API naming for endpoints

--- backend/auctions/urls.py
+++ backend/auctions/urls.py
@@ -3,30 +3,30 @@ from . import views
 
 urlpatterns = [
     path(
-        'all_auctions/',
+        'all-auctions',
         views.AuctionListView.as_view(),
         name='all_auctions'),
     path(
-        'particular_auction/<int:pk>/',
+        'my-auction/<int:pk>',
         views.AuctionOwnerDetailView.as_view(),
         name='manage_auction'),
     path(
-        'my_auctions/',
+        'my-auctions',
         views.AuctionOwnerView.as_view(),
         name='my_auctions'
     ),
     path(
-        "auction_detail/<int:pk>/",
+        "auction-detail/<int:pk>",
         views.AuctionDetailView.as_view(),
         name='auction_detail'
     ),
     path(
-        "auctions_per_user",
+        "auctions-per-user",
         views.AuctionsPerUserView.as_view(),
         name='auctions_per_user'
     ),
     path(
-        "filter_auction/",
+        "filter-auctions",
         views.AuctionFilterView.as_view(),
         name='filter_auction'
     ),
