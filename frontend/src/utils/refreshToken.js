50448799a1f2812c96ed8e2cd8aa77c4d997d6c3
andrew <andry925@ukr.net>
2024-07-24 10:44:31 +0300

    feat: implement updateTokens utility

---
+++ frontend/src/utils/refreshToken.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const updateTokens = () => {
+  axios.post('http://127.0.0.1:8000/api/token-refresh',
+    { refresh: localStorage.getItem('refreshToken') },
+    {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+    .then(response => {
+      const newAccessToken = response.data.access;
+      const newRefreshToken = response.data.refresh;
+
+      localStorage.setItem('accessToken', newAccessToken);
+      localStorage.setItem('refreshToken', newRefreshToken);
+    })
+    .catch(error => {
+      console.error('Ошибка при обновлении токена:', error);
+    });
+};
